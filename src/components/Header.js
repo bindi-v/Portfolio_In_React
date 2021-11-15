e15b0beba8eed2a6bb0b4e610fe710f32a40b3ab
bindi-v <bindi.vaghela@gmail.com>
2021-11-15 18:47:04 -0500

    added Header component and it's style

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="home">
+        <div className="max-width">
+            <div className="home-content">
+                <div className="text-1">Hello, My Name Is </div>
+                <div className="text-2">Bindu Vaghela</div>
+                <div className="text-3">And I am a<span> Web Developer</span></div>
+                
+            </div>
+        </div>  
+        </div>
+    )
+}
+
+export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,12 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container">
-    <a className="navbar-brand" href="#">My Portfolio</a>
+    <a className="navbar-brand logo" href="#">Portfo<span>lio.</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}} />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto ">
+      <ul className="navbar-nav ">
         <li className="nav-item active">
           <a className="nav-link" href="#">Home<span className="sr-only"></span></a>
         </li>
