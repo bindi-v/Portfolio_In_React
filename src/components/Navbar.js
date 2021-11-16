44d03f647ff590b3c7e3b808e8042b2918c4999c
bindi-v <bindi.vaghela@gmail.com>
2021-11-15 22:51:42 -0500

    modified anchor tags for href link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home<span className="sr-only"></span></a>
+          <a className="nav-link" href="#home">Home<span className="sr-only"></span></a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#about">About</a>
