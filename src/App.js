32a39b931b2094bc7f5daf3b66e1e8291c9cfe8c
bindi-v <bindi.vaghela@gmail.com>
2021-11-15 21:42:43 -0500

    added About section with styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
 import Header from "./components/Header";
 import Footer from './components/Footer';
+import About from './components/About';
+
 
 function App() {
+  
   return (
     <>
     <Navbar />
     <Header />
+    <About />
     <Footer />
     </>
   );
---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import MyImg from "../assets/My_Img.jpeg";
+const About = () => {
+    return (
+         <section className="about" >
+        <div className="max-width">
+            <h2 className="title">About Me</h2>
+            <div className="about-content">
+                <div className=" column left">
+                    <img src={ MyImg } alt="MyImg" />
+                </div>
+                <div className="column right">
+                    <div class="text">I'm Bindu and I'm a <span>Web Developer</span></div>
+                    <p> I am a full stack Web Developer with a Mathematics Background.
+                         Recently earned a certificate in full stack web development from the
+                        Georgia Institute of Technology, with newly developed skills in JavaScript, CSS, React.js, 
+                         and responsive web design.</p>
+                    <a href="https://docs.google.com/document/d/1zwoyfyCn3x1szalvzQKTpUc5ZeinJQt03M61w0cRyOM/edit?usp=sharing">My CV</a>
+                </div>
+            </div>
+        </div>   
+        </section>
+    )
+}
+
+export default About
