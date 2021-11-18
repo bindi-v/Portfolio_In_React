1ed5547a1068b99965600403bb8252f9588f43fd
bindi-v <bindi.vaghela@gmail.com>
2021-11-18 02:38:04 -0500

    modified component Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,60 +3,60 @@ import React from 'react'
 const Skills = () => {
     return (
         <div>
-            <section class="skills" id="skills">
-        <div class="max-width">
-            <h2 class="title">My Skills</h2>
-            <div class="skills-content">
-                <div class="column left">
-                    <div class="text">My creative skills and experiences</div>
+            <section className="skills" id="skills">
+        <div className="max-width">
+            <h2 className="title">My Skills</h2>
+            <div className="skills-content">
+                <div className="column left">
+                    <div className="text">My creative skills and experiences</div>
                   <p>HTML/CSS, GIT,
                     Analytical,
                     Responsive design,
                     JavaScript, NodeJS, ExpressJS,
-                    MYSQL, Handlebars, MongoDB, React
-                    Interpersonal,
+                    MYSQL, Handlebars, MongoDB, React, 
+                    GraphQL, Interpersonal,
                     Testing and debugging,
                     Back-end basics and
                     Search engine optimization
                     </p>
 
                 </div>
-                <div class="column right">
-                    <div class="bars">
-                        <div class="info">
+                <div className="column right">
+                    <div className="bars">
+                        <div className="info">
                             <span>HTML, CSS, JavaScript</span>
                         </div>
-                        <div class="line html"></div>
+                        <div className="line html"></div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>GIT</span>
                         </div>
-                        <div class="line html"></div>
+                        <div className="line html"></div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>NodeJS</span>
                         </div>
-                        <div class="line css"></div>
+                        <div className="line css"></div>
                     </div>                    
-                    <div class="bars">
-                        <div class="info">
-                            <span>Express & MYSQL</span>
+                    <div className="bars">
+                        <div className="info">
+                            <span>Express,  MYSQL</span>
                         </div>
-                        <div class="line js"></div>
+                        <div className="line js"></div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>MongoDB</span>
                         </div>
-                        <div class="line js"></div>
+                        <div className="line js"></div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
-                            <span>React</span>
+                    <div className="bars">
+                        <div className="info">
+                            <span>React,  GraphQL</span>
                         </div>
-                        <div class="line js"></div>
+                        <div className="line js"></div>
                     </div>
                 </div>
             </div>
