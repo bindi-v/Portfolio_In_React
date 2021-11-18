3c33864a3adfe243bcd8fc2abf05ab8f38df8971
bindi-v <bindi.vaghela@gmail.com>
2021-11-18 02:13:09 -0500

    change icon's size and clean cole in component Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,15 +9,13 @@ const Home = () => {
                 <div className="text-1">Hello, My Name Is </div>
                 <div className="text-2">Bindu Vaghela</div>
                 <div className="text-3">And I am a<span> Web Developer</span></div>
-                
-            </div>
+             </div>
             <div className=" info" >
-        <div className="head"><a href="https://github.com/bindi-v" target="_blank" rel="noopener noreferrer"><FaGithub style={{ color: "orange"}} />  GitHub</a></div>
-        <div className="head"> <a href="https://www.linkedin.com/in/bindu-vaghela-123826214/" target="_blank" rel="noopener noreferrer"><FaLinkedin  style={{ color: "orange"}} />  LinkedIn</a></div>
+        <div className="head"><a href="https://github.com/bindi-v" target="_blank" rel="noopener noreferrer"><FaGithub size={20} style={{ color: "orange"}} /></a></div>
+        <div className="head"> <a href="https://www.linkedin.com/in/bindu-vaghela-123826214/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={20} style={{ color: "orange"}} /></a></div>
         </div>
         </div>  
         </div>
-        
         </div>
     )
 }
