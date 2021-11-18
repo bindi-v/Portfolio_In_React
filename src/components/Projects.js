5a0babf0ac68912cda692a68f55c55a1da2c3d80
bindi-v <bindi.vaghela@gmail.com>
2021-11-18 01:55:53 -0500

    added github links for related projects in project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ import Project6Img from "../assets/WorkdayScheduler.gif";
 import Project7Img from "../assets/WeatherDashboard.png";
 import Project8Img from "../assets/img.webp";
 import Project9Img from "../assets/TwoCityComparisonTool.png";
-
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
     
@@ -27,63 +27,63 @@ const Projects = () => {
                     <div className="box">
                     <img src={ Project1Img } alt="Homeschool HookUp homepage screenshot" />
                     <div className="text"><a href="https://shrieking-mummy-96456.herokuapp.com/" target="_blank" rel="noopener noreferrer" >HomeSchool_HookUp</a></div>
-                    
+                    <a href="https://github.com/bindi-v/HomeSchool_HookUp" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project2Img } alt="Note Taker homepage screenshot" />
                     <div className="text"><a href="https://blooming-brook-52468.herokuapp.com/" target="_blank" rel="noopener noreferrer">Note Taker</a></div>
-                   
+                    <a href="https://github.com/bindi-v/Note_Taker" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project3Img } alt="Team Profile Generating homepage screenshot" />
                     <div className="text"><a href="https://github.com/bindi-v/Team_Profile_Generator" target="_blank" rel="noopener noreferrer">Team Profile Generator</a></div>
-                   
+                    <a href="https://github.com/bindi-v/Team_Profile_Generator" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project4Img } alt="code quiz gif" />
                     <div className="text"><a href="https://bindi-v.github.io/Code_Quiz/" target="_blank" rel="noopener noreferrer">Code Quiz</a></div>
-                    
+                    <a href="https://github.com/bindi-v/Code_Quiz" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project5Img } alt="Password-Generator img" />
                     <div className="text"><a href="https://bindi-v.github.io/Password-Generator/" target="_blank" rel="noopener noreferrer">Password-Generator</a></div>
-                    
+                    <a href="https://github.com/bindi-v/Password-Generator" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project6Img } alt="Work Day Scheduler img" />
                     <div className="text"><a href="https://bindi-v.github.io/Work_Day_Scheduler/" target="_blank" rel="noopener noreferrer">Work Day Scheduler</a></div>
-                    
+                    <a href="https://github.com/bindi-v/Work_Day_Scheduler" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project7Img } alt="Weather_Dashboard img" />
                     <div className="text"><a href="https://bindi-v.github.io/Weather_Dashboard/" target="_blank" rel="noopener noreferrer">Weather_Dashboard</a></div>
-                    
+                    <a href="https://github.com/bindi-v/Weather_Dashboard" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project8Img } alt="Professional Readme.md Generator" />
                     <div className="text"><a href="https://github.com/bindi-v/Professional_README_Generator" target="_blank" rel="noopener noreferrer">Professional Readme.md Generator</a></div>
-                    
+                    <a href="https://github.com/bindi-v/Professional_README_Generator" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 <div className="card item">
                     <div className="box">
                     <img src={ Project9Img } alt="TwoCityComparisonTool img" />
                     <div className="text"><a href="https://pgandhi21.github.io/Two-City-Comparison-Tool/" target="_blank" rel="noopener noreferrer">Two-City-Comparison-Tool</a></div>
-                    
+                    <a href="https://github.com/bindi-v/Two-City-Comparison-Tool" target="_blank" rel="noopener noreferrer"><FaGithub  size={28} style={{ color: "#fff" }} /></a>
                 </div>
                 </div>
                 </OwlCarousel>
